77e3da1998dc16678587f9bb720db2c97cb99ec7
Lauren Ashpole <lauren@laurenashpole.com>
2019-05-25 23:35:20 -0400

    Updated demo page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './product.css';
 
+// TODO: Switch to module when published
+import InnerImageZoom from './InnerImageZoom';
+
 const Product = () => {
   return(
     <section className="product">
       <div className="product__img">
-
+        <InnerImageZoom src="/images/unsplash-1.jpg" zoomSrc="/images/unsplash-1-large.jpg" />
       </div>
 
       <div className="product__details">
@@ -18,7 +21,7 @@ const Product = () => {
           <li>Zoom + pan on hover and zoom + drag on touch</li>
           <li>Can be used with other popular React components</li>
           <li>Supports responsive images</li>
-          <li>Photo credit: <a href="https://unsplash.com/photos/jQ0CTkvlz2U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Micheile Henderson</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a></li>
+          <li>Photo credit: <a href="https://unsplash.com/photos/jQ0CTkvlz2U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="noopener noreferrer">Micheile Henderson</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="noopener noreferrer">Unsplash</a></li>
         </ul>
       </div>
     </section>
