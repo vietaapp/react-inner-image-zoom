a88419d5daff6a76ae4964e1a8b2c505fd4f38c8
Lauren Ashpole <lauren@laurenashpole.com>
2019-05-25 21:46:07 -0400

    Updates

---
+++ precache-manifest.b968628ab6b408914a3a45df0f35e6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805b41be95cd4729a614c6039726776a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f4b9ae5624c9cd18ae9",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6d45f540739c211c8f31",
+    "url": "/static/js/2.b0d133b6.chunk.js"
+  },
+  {
+    "revision": "2f4b9ae5624c9cd18ae9",
+    "url": "/static/js/main.898ec344.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/pages/Demos.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Demos = () => {
-  return(
-    <div>Demos</div>
-  );
-};
-
-export default Demos;
