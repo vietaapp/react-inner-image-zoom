a85ca1252158a5f056473fd99d60eedc1c6f19c9
Lauren Ashpole <lauren@laurenashpole.com>
2019-05-29 21:48:35 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-inner-image-zoom/precache-manifest.5c9cc4a3993de577f537a78b3d990d8a.js"
+  "/react-inner-image-zoom/precache-manifest.e83eb166bc4a4c73b75c4cef8ec8be3d.js"
 );
 
 self.addEventListener('message', (event) => {
