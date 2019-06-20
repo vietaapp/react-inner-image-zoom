8fce456e5f1fc2c79aff86277a1ff836d9011cdd
Lauren Ashpole <lauren@laurenashpole.com>
2019-06-19 20:13:07 -0400

    Updates

---
+++ precache-manifest.f70f0626b9edf9f9f83201576c931703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6299d756b4d419bc837c248e83d7fe1d",
+    "url": "/react-inner-image-zoom/index.html"
+  },
+  {
+    "revision": "14925b03420823d7a129",
+    "url": "/react-inner-image-zoom/static/css/2.1c5c90a3.chunk.css"
+  },
+  {
+    "revision": "c63b7815ef2f2fc5d407",
+    "url": "/react-inner-image-zoom/static/css/main.b9939b22.chunk.css"
+  },
+  {
+    "revision": "14925b03420823d7a129",
+    "url": "/react-inner-image-zoom/static/js/2.ae3844cb.chunk.js"
+  },
+  {
+    "revision": "c63b7815ef2f2fc5d407",
+    "url": "/react-inner-image-zoom/static/js/main.11247986.chunk.js"
+  },
+  {
+    "revision": "f3b3cd217e5ed5093bcf",
+    "url": "/react-inner-image-zoom/static/js/runtime~main.e502cfd5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-inner-image-zoom/precache-manifest.a2ba0876c2cb5cb5d782f15618b28284.js"
+  "/react-inner-image-zoom/precache-manifest.f70f0626b9edf9f9f83201576c931703.js"
 );
 
 self.addEventListener('message', (event) => {
