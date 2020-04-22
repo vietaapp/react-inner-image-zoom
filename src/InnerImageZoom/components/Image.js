1220de8a8b73791b6b4980d5ff22af818dc66ab1
Justas Palumickas <jpalumickas@gmail.com>
2020-04-22 15:49:17 +0300

    Allow to pass other props to image

--- src/InnerImageZoom/components/Image.js
+++ src/InnerImageZoom/components/Image.js
@@ -1,55 +1,42 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-const Image = (props) => {
-  const {
-    src,
-    srcSet,
-    sizes,
-    sources,
-    alt,
-    isZoomed
-  } = props;
+const Image = ({
+  sources,
+  isZoomed,
+  className,
+  ...rest
+}) => (
+  <Fragment>
+    {sources && sources.length > 0 ? (
+      <picture>
+        {sources.map((source, i) => {
+          return(
+            <Fragment key={i}>
+              {source.srcSet &&
+                <source
+                  srcSet={source.srcSet}
+                  sizes={source.sizes}
+                  media={source.media}
+                  type={source.type}
+                />
+              }
+            </Fragment>
+          );
+        })}
 
-  return(
-    <Fragment>
-      {sources && sources.length > 0 ? (
-        <picture>
-          {sources.map((source, i) => {
-            return(
-              <Fragment key={i}>
-                {source.srcSet &&
-                  <source
-                    srcSet={source.srcSet}
-                    sizes={source.sizes}
-                    media={source.media}
-                    type={source.type}
-                  />
-                }
-              </Fragment>
-            );
-          })}
-
-          <img
-            className={`iiz__img ${isZoomed ? 'iiz__img--invisible' : ''}`}
-            src={src}
-            srcSet={srcSet}
-            sizes={sizes}
-            alt={alt}
-          />
-        </picture>
-      ) : (
         <img
-          className={`iiz__img ${isZoomed ? 'iiz__img--invisible' : ''}`}
-          src={src}
-          srcSet={srcSet}
-          sizes={sizes}
-          alt={alt}
+          className={`iiz__img ${className} ${isZoomed ? 'iiz__img--invisible' : ''}`}
+          {...rest}
         />
-      )}
-    </Fragment>
-  );
-};
+      </picture>
+    ) : (
+      <img
+        className={`iiz__img ${className} ${isZoomed ? 'iiz__img--invisible' : ''}`}
+      />
+    )}
+  </Fragment>
+);
 
 Image.propTypes = {
   src: PropTypes.string.isRequired,
